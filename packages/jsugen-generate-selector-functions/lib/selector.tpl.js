bff3629030d6634c3c67d9ed3d798c6265852c73
Stephan Herzog <stephan.herzog_ext@scout24.com>
2019-02-28 10:51:30 +0100

    feat: support passing default return values to selectors

--- packages/jsugen-generate-selector-functions/lib/selector.tpl.js
+++ packages/jsugen-generate-selector-functions/lib/selector.tpl.js
@@ -1,7 +1,7 @@
 import camelCase from 'lodash.camelcase';
 
 export default ({ path }) => `
-export function ${camelCase(`by_${path}`)}(property) {
-  return get(property, '${path}');
+export function ${camelCase(`by_${path}`)}(property, defaultReturn) {
+  return get(property, '${path}', defaultReturn);
 }
 `;
