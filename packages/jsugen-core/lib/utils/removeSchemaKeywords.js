0e1af612516c106743d3c1c6a8412d94209ddf04
Stephan Herzog <sthzgvie@gmail.com>
2019-04-26 18:11:33 +0200

    fix: Fix prettier script in root package.json

--- packages/jsugen-core/lib/utils/removeSchemaKeywords.js
+++ packages/jsugen-core/lib/utils/removeSchemaKeywords.js
@@ -1,7 +1,9 @@
 import { JSON_SCHEMA_V4_KEYWORDS } from '../constants';
 
 function removeSchemaKeywords(pathTokens) {
-  return pathTokens.filter(segment => !JSON_SCHEMA_V4_KEYWORDS.includes(segment));
+  return pathTokens.filter(
+    segment => !JSON_SCHEMA_V4_KEYWORDS.includes(segment)
+  );
 }
 
 export default removeSchemaKeywords;
