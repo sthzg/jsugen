49b4a649a905e0d55ff273d6780745993ec374b6
Stephan Herzog <sthzgvie@gmail.com>
2019-05-07 21:13:03 +0200

    build: Try fixing generation of package lockfiles on CI

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+lerna publish --conventional-commits --yes --no-push
+lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+git add **/package-lock.json
+git commit --amend --no-edit
+VERSION=`node -pe "require('$PWD/lerna.json').version;"`
+git tag -f "v${VERSION}"
+git push --follow-tags
