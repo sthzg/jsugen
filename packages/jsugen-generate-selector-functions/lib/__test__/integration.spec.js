12871599290a58621c506150809caaa6764690a7
Stephan Herzog <sthzgvie@gmail.com>
2019-05-12 13:54:14 +0200

    feat: Support resolving JSON schema `definitions`

--- packages/jsugen-generate-selector-functions/lib/__test__/integration.spec.js
+++ packages/jsugen-generate-selector-functions/lib/__test__/integration.spec.js
@@ -1,10 +1,11 @@
 import schema from '@sthzg/jsugen-core/lib/testResources/example.schema';
 import generate from '../index';
 
-test('generate-selector-functions integration test', () => {
+test('generate-selector-functions integration test', done => {
   expect.assertions(1);
 
   generate({ schema }).subscribe(output => {
     expect(output).toMatchSnapshot();
+    done();
   });
 });
