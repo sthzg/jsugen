3171e4a876a8364f6c40db5f481288cddf478352
Stephan Herzog <sthzgvie@gmail.com>
2019-06-19 01:54:49 +0200

    chore(core): Refine package structure

--- packages/jsugen-core/lib/errors/index.js
+++ packages/jsugen-core/lib/errors/index.js
@@ -1,3 +1,2 @@
-export { ERROR_CODES } from './constants';
 export { GenerateFunctionNotFoundError } from './GenerateFunctionNotFoundError';
 export { ModuleNotFoundError } from './ModuleNotFoundError';
--- packages/jsugen-core/lib/output/toTemplateRawStringReducer.js
+++ packages/jsugen-core/lib/output/toTemplateRawStringReducer.js
@@ -1,5 +1,5 @@
-import { NEWLINE } from './constants';
-import { byTemplateRaw } from './selectors';
+import { NEWLINE } from '../constants';
+import { byTemplateRaw } from '../selectors';
 
 export function toTemplateRawStringReducer(result, current) {
   return [result, byTemplateRaw(current)].join(NEWLINE);
