b0b0dcf91575a97058739f91948c651104564659
Stephan Herzog <sthzgvie@gmail.com>
2019-06-26 21:12:12 +0200

    fix(generate-enums): separate camel-cased keys correctly

--- packages/jsugen-core/lib/utils/array.js
+++ packages/jsugen-core/lib/utils/array.js
@@ -1,4 +1,5 @@
 import { castArray, merge, isEmpty, isString, isNumber } from 'lodash-es';
+import { toUpperSnakeCase } from './format';
 
 /**
  * Returns the result of `fn(...args)` if predicate is truthy or undefined.
@@ -8,7 +9,7 @@ export function maybeItem(predicate, fn, ...args) {
 }
 
 /**
- * Returns a key-value object with input array values as upper-cased keys.
+ * Returns a key-value object with input array values as upper-snake-cased keys.
  */
 export function arrayToEnum(array) {
   const isNonEmptyStringOrNumber = value =>
@@ -17,7 +18,7 @@ export function arrayToEnum(array) {
   return castArray(array)
     .filter(isNonEmptyStringOrNumber)
     .reduce((result, value) => {
-      const key = `${value}`.toUpperCase();
+      const key = toUpperSnakeCase(value);
 
       return merge({}, result, { [key]: value });
     }, {});
