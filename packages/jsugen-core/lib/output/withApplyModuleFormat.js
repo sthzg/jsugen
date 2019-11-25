ec3bbac54e4fbe08dc013596476ca094c7fe92a4
Stephan Herzog <stephan.herzog_ext@scout24.com>
2019-11-25 21:46:47 +0100

    fix(core): Provide minimal babel config to ensure transpilation

--- packages/jsugen-core/lib/output/withApplyModuleFormat.js
+++ packages/jsugen-core/lib/output/withApplyModuleFormat.js
@@ -2,11 +2,23 @@ import { get } from 'lodash-es';
 import * as babel from '@babel/core';
 import { ModuleFormat } from '../enums';
 
-export function withApplyModuleFormat(writeConfig, babelConfig) {
+/**
+ * For now a minimal local configuration to ensure that ESM can be
+ * transpiled into CJS.
+ */
+const BABEL_CONFIG = {
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-syntax-export-default-from',
+    '@babel/plugin-transform-modules-commonjs',
+  ],
+};
+
+export function withApplyModuleFormat(writeConfig) {
   return content => {
     switch (writeConfig.moduleFormat) {
       case ModuleFormat.CJS: {
-        return get(babel.transformSync(content, babelConfig), 'code');
+        return get(babel.transformSync(content, BABEL_CONFIG), 'code');
       }
       default: {
         return content;
